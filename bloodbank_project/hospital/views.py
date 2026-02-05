7d0265210c4278a5581cd4fc6719f6fdd6575b59
mitrata57 <meetrata71@gmail.com>
2026-02-05 21:41:09 +0545

    Add templates

--- bloodbank_project/hospital/views.py
+++ bloodbank_project/hospital/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def hospital_register(request):
-    return render(request, 'hospital/register,html')
+    return render(request, 'hospital/register.html')
 
 def dashboard(request):
     return render(request, 'hospital/dashboard.html')
