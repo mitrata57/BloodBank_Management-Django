ffb5d0527435f250a19552bb0d3d98ff0e9641a6
mitrata57 <meetrata71@gmail.com>
2026-02-10 12:26:25 +0545

    Correct Code

--- bloodbank_project/accounts/views.py
+++ bloodbank_project/accounts/views.py
@@ -1,12 +1,10 @@
 from django.shortcuts import render
 
-# Remove the home() function - it belongs in core app!
-# Remove the about() function - it belongs in core app!
+
 
 def register(request):
-    """Registration page - we'll build this properly tomorrow"""
+    
     return render(request, 'accounts/register.html')
 
 def login_view(request):
-    """Login page - we'll build this tomorrow"""
     return render(request, 'accounts/login.html')
